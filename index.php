50e688039e18143eb674e4da3683a5ad6b87a139
toniyansyah4 <pembuatanwebsite61@gmail.com>
2021-03-13 16:53:18 +0700

    UPDATE FOOTER

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ $data = [
     }
     ?>
     <div class="menu">
-        <h1>@toniyansyah4</h1>
+        <h3>@toniyansyah4</h3>
     </div>
     <script src="js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
